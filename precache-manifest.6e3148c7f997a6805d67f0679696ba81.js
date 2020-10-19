44d63e59339e502b171706801ec958a1b37cb7cd
klappy <christopher@klapp.name>
2020-10-19 12:15:18 -0400

    Updates

---
+++ precache-manifest.6e3148c7f997a6805d67f0679696ba81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b893fe51093b624afab27fee3515f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a316d3539109fcafe497",
+    "url": "/static/css/2.f290e260.chunk.css"
+  },
+  {
+    "revision": "dd70f52757f5f33810b4",
+    "url": "/static/css/main.7e72c65b.chunk.css"
+  },
+  {
+    "revision": "a316d3539109fcafe497",
+    "url": "/static/js/2.f6191d55.chunk.js"
+  },
+  {
+    "revision": "dd70f52757f5f33810b4",
+    "url": "/static/js/main.4bac1e09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
